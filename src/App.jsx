21ad5aa58a69cad26075213c9e3b99678c3e0364
tratchapong <tratchapong@gmail.com>
2024-06-23 17:56:21 +0700

    mui with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Button } from "@mui/material"
 
 function App() {
 
   return (
     <>
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Hello world! from Codecamp
     </h1>
+    <Button variant="contained"
+      sx = {{ color: 'red'}}
+      className="bg-primary text-lime-400 ms-5 my-4"
+    >Hello world</Button>
     </>
   )
 }
